54301e217793055703431090606a05db392f3618
Clinton Owen <clint@clintonowen.com>
2019-05-20 18:31:40 -0500

    Create basic Welcome Screen component

---
+++ src/screens/Welcome/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+export class WelcomeScreen extends Component {
+  render () {
+    return (
+      <View>
+        <Text>
+          Words Unbound
+        </Text>
+      </View>
+    );
+  }
+}
+
+export default WelcomeScreen;
